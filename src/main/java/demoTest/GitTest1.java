c85e9001ceb0fc0bb14248c90fb474d3f2b505e7
kziaamir <kasrabist@gmail.com>
2021-10-07 15:51:40 -0500

    4th commit

--- src/main/java/demoTest/GitTest1.java
+++ src/main/java/demoTest/GitTest1.java
@@ -1,5 +1,13 @@
 package demoTest;
 
 public class GitTest1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("git test");
+		System.out.println("git test1");
+		System.out.println("git test2");
+		System.out.println("git test3");
+	}
 
 }
